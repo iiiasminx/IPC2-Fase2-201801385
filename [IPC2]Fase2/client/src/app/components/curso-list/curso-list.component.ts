05270da964d44ce810f56eaa847713b655a5ee05
Yasmin Monterroso <yasminmonterrosoe@gmail.com>
2019-10-26 21:47:36 -0600

    :D
    :100:

--- [IPC2]Fase2/client/src/app/components/curso-list/curso-list.component.ts
+++ [IPC2]Fase2/client/src/app/components/curso-list/curso-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {CursosService} from '../../services/cursos.service';
+
+import { CursosService } from '../../services/cursos.service';
+import { AsignacionesService } from '../../services/asignaciones.service'
+import { SeccionesService } from '../../services/secciones.service';
+
+import { Curso } from 'src/app/models/curso';
 
 @Component({
   selector: 'app-curso-list',
@@ -9,32 +14,80 @@ import {CursosService} from '../../services/cursos.service';
 export class CursoListComponent implements OnInit {
 
   cursos: any = []
-  constructor(private cursosService: CursosService) { }
+  asignaciones: any = []
+  secciones: any = []
 
-  ngOnInit() {    
+  curso: Curso = {
+    id_curso: 0,
+    cur_nombre: "",
+    cur_semestre: 0,
+    cur_year: 0
+  }
+
+  constructor(private cursosService: CursosService,
+    private asignacionservice: AsignacionesService,
+    private seccionesservice: SeccionesService) { }
+
+  ngOnInit() {
     this.obteniendoJuegos();
   }
 
-  obteniendoJuegos(){
+  obteniendoJuegos() {
     this.cursosService.getCursos().subscribe(
       res => {
-        console.log(res);
+        //console.log(res);
         this.cursos = res;
       },
       err => console.error(err)
     )
-  }
 
-  eliminando(id : number){
-    let sid = id.toString();
-    console.log(sid);
-    this.cursosService.eliminarCurso(sid).subscribe(
+    this.asignacionservice.getAsigns().subscribe(
       res => {
-        console.log(res);
-        this.obteniendoJuegos();
+        this.asignaciones = res;
       },
       err => console.error(err)
     )
+
+    this.seccionesservice.getSecciones().subscribe(
+      res => {
+        this.secciones = res;
+      },
+      err => console.error(err)
+    )
+  }
+
+  eliminando(id: number) {
+
+    let sid = id.toString();
+    console.log("id del curso: " + sid);
+
+    let ningunoasignado = true;
+
+    for (let secioon of this.secciones) {
+      if (secioon.id_curso == id) {
+        //alert("hay una seccion en el curso");
+        
+        for(let asignacioon of this.asignaciones){
+          if(asignacioon.id_seccion == secioon.id_seccion){            
+            ningunoasignado = false;
+          }
+        }
+      }
+    }
+
+    if(ningunoasignado == false){
+      alert("Hay alumanos asignados a este curso!! D:")
+    }
+
+    if (ningunoasignado) {
+      this.cursosService.eliminarCurso(sid).subscribe(
+        res => {
+          console.log(res);
+          this.obteniendoJuegos();
+        },
+        err => console.error(err)
+      )
+    }
   }
 
 }
